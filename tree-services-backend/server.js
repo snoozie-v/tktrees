15162f1c439cd406eb5e92022ffc3d187b1698f1
snoozie-v <gettingsleepyz@gmail.com>
2024-09-02 14:21:21 -0500

    removing server

--- tree-services-backend/server.js
+++ tree-services-backend/server.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const app = express();
-const port = process.env.PORT || 3000;
-const path = require('path');
-const cors = require('cors');
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+// const express = require('express');
+// const nodemailer = require('nodemailer');
+// const app = express();
+// const port = process.env.PORT || 3000;
+// const path = require('path');
+// const cors = require('cors');
+// app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, '../public')));
 
-// Nodemailer setup
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {}
-    }
+// // Nodemailer setup
+// const transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     secure: false,
+//     auth: {}
+//     }
     
-});
+// });
 
-app.post('/submit-quote', (req, res) => {
-    const { name, email, phone, service, description } = req.body;
+// app.post('/submit-quote', (req, res) => {
+//     const { name, email, phone, service, description } = req.body;
     
-    const mailOptions = {
-        from: 'leadflow975@gmail.com',
-        to: 'leadflow975@gmail.com',
-        subject: 'New Quote Request',
-        text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nService: ${service}\nDescription: ${description}`
-    };
+//     const mailOptions = {
+//         from: 'leadflow975@gmail.com',
+//         to: 'leadflow975@gmail.com',
+//         subject: 'New Quote Request',
+//         text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nService: ${service}\nDescription: ${description}`
+//     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            res.status(500).send('Error sending email. Please try again later.');
-            return;
-        }
-        console.log('Message sent: %s', info.messageId);
-        res.sendFile(path.join(__dirname, '../public/thank-you.html'));
-    });
-});
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             console.log(error);
+//             res.status(500).send('Error sending email. Please try again later.');
+//             return;
+//         }
+//         console.log('Message sent: %s', info.messageId);
+//         res.sendFile(path.join(__dirname, '../public/thank-you.html'));
+//     });
+// });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+// });
