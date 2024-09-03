8b4c0de4d6c9da5ff0c6388ab538bb4a3356d628
snoozie-v <gettingsleepyz@gmail.com>
2024-09-02 22:35:12 -0500

    reintroducing backEnd

---
+++ backEnd/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true}))
+
+// Serve static files from the 'public' folder which is a sibling to 'backEnd'
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Route for root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.post('/submit-quote', (req, res) => {
+    const { name, email, phone, service, description } = req.body
+    console.log(req.body)
+    res.sendFile(path.join(__dirname, '..', 'public/thank-you.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
