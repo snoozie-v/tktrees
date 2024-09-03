c55e330f93a2557c7580c6c903bf24d79e808e97
snoozie-v <gettingsleepyz@gmail.com>
2024-09-03 17:27:31 -0500

    the issue was not the css pointer, this time i looked at the paths

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const nodemailer = require('nodemailer');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
@@ -8,16 +7,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Nodemailer setup
-// const transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 587,
-//     secure: false, // Use TLS but not SSL
-//     auth: {
-//         user: 'your-email@gmail.com',
-//         pass: 'your-app-password-or-oauth2-token'
-//     }
-// });
+
 
 app.post('/submit-quote', (req, res) => {
     const { name, email, phone, service, description } = req.body;
